31a18b4402f872ef5234a669e33579d26eaf490c
Röhr <lukas.roehr@vc-g.de>
2023-04-27 09:24:26 +0200

    added component InputText

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputText from './components/elements/InputText';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          This site is under construction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" name="netto" value="0,00" />
+        <input type="text" name="hours" value="1" />
+        <InputText>test</InputText>
       </header>
     </div>
   );
---
+++ src/NettInputInterface.ts
@@ -0,0 +1,4 @@
+export default interface NettoInputInterface {
+    name: string;
+    value: number;
+}
