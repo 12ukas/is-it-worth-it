e888931a404e4685aad3cc9d57961651e58e52d6
Röhr <lukas.roehr@vc-g.de>
2023-04-28 16:51:57 +0200

    added styled-components, added needed functionality, added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,57 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import styled from "styled-components";
+
 import './App.css';
 import Input from './elements/Input';
 
+
+const MinimumCostsResult = styled.span`
+  text-decoration: underline;
+  font-weight: bold;
+  font-size: xx-large;
+  color: red;
+`;
+
+const HourlyWageResult = styled.span`
+  text-decoration: underline;
+  font-weight: bold;
+  font-size: xx-large;
+  color: green;
+`;
+
+
 function App() {
-  const [nettoEarnings, setNettoEarnings] = useState<number>(0.00);
-  const [workingHours, setworkingHours] = useState<number>(40);
+  const [nettoEarnings, setNettoEarnings] = useState<number>(0);
+  const [workingHoursDaily, setWorkingHoursDaily] = useState<number>(8);
+  const [projectDurationHours, setProjectDurationHours] = useState<number>(0);
 
-  const calculate = (nettoEarnings: number, workingHours: number) => {
-    if (nettoEarnings > 0 && workingHours > 0) {
-      const result = nettoEarnings / workingHours;
-      return result.toFixed(2);
-    }
+  const calculateMinimumCosts = (projectDurationHours: number, hourlyWageResult: number) => {
+    if (projectDurationHours <= 0 || hourlyWageResult <= 0) return 0;
 
-    return "";
+    const result = projectDurationHours * hourlyWageResult;
+
+    return parseFloat(result.toFixed(2));
   }
-  const result = calculate(nettoEarnings, workingHours);
+
+  const calculateHourlyWage = (workingHoursDaily: number, nettoEarnings: number) => {
+    if (nettoEarnings <= 0 || workingHoursDaily <= 0) return 0;
+
+    const result = nettoEarnings / calculateMonthlyWorkingHours(workingHoursDaily);
+
+    return parseFloat(result.toFixed(2));
+  }
+
+  const calculateMonthlyWorkingHours = (workingHoursDaily: number) => {
+    if (workingHoursDaily <= 0) return 0;
+
+    const daysPerWeek = 5;
+    const weeksPerMonth = 4;
+
+    return workingHoursDaily * daysPerWeek * weeksPerMonth;
+  }
+
+  const hourlyWageResult = calculateHourlyWage(workingHoursDaily, nettoEarnings);
+  const minimumCostsResult = calculateMinimumCosts(projectDurationHours, hourlyWageResult);
 
   return (
     <div className="App">
@@ -24,14 +60,21 @@ function App() {
         Is the upcoming workload worth it or should you rather hire external? <br />
         Enter your net earnings and the number of hours required for the project.
         </p>
-        <section>
-          <Input id="netto-earnings" value={nettoEarnings} setter={setNettoEarnings} step="any"/>
-          <label htmlFor="netto-earnings">€</label>
-          <br />
-          <Input id="working-hours" value={workingHours} setter={setworkingHours} step="1"/>
-          <label htmlFor="netto">hrs</label>
 
-          {result.trim() !== "" ? <p>The project will cost you {result} € per hour</p> : <p>&nbsp;</p>}
+        <section>
+          <Input id="netto-earnings" value={nettoEarnings} setter={setNettoEarnings} step="0.01" label="Net earnings per month"/>
+          {/* <label htmlFor="netto-earnings">€</label> */}
+
+          <Input id="working-hours" value={workingHoursDaily} setter={setWorkingHoursDaily} step="1" label="Daily working time"/>
+          {/* <label htmlFor="working-hours">working hours daily</label> */}
+
+          <Input id="project-hours" value={projectDurationHours} setter={setProjectDurationHours} step="1" label="Project length in hours"/>
+          {/* <label htmlFor="netto">needed project hours</label> */}
+        </section>
+
+        <section>
+          {hourlyWageResult > 0 ? <p>You earn <HourlyWageResult>{hourlyWageResult}</HourlyWageResult> € per hour.</p> : <p>&nbsp;</p>}
+          {minimumCostsResult > 0 ? <p>If the external costs in total more than <MinimumCostsResult>~{minimumCostsResult}</MinimumCostsResult> €, you should consider doing it yourself. Otherwise hire an external!</p> : <p>&nbsp;</p>}
         </section>
       </header>
     </div>
--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -1,24 +1,74 @@
+import styled from "styled-components";
+
+const LabelStyles = styled.label`
+    color: #9e9e9e;
+    font-size: 16px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    pointer-events: none;
+    transform: translateX(-1.2rem) translateY(55px);
+    transition: all 0.2s ease-in-out;
+    text-align: right;
+`;
+
+const InputStyles = styled.input`
+  color: white;
+  font-size: 2rem;
+  padding: 0;
+  padding-bottom: .3em;
+  padding-right: .4rem;
+  margin: 0.5em;
+  text-align: right;
+  width: 12rem;
+  background: none;
+  border: none;
+  border-bottom: 2px solid lightgray;
+
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance:textfield;
+  outline: none;
+
+  &:focus + ${LabelStyles},
+  &:valid + ${LabelStyles} {
+    transform: translateX(-1.2rem) translateY(0);
+  }
+`;
+
+const FormGroupStyles = styled.div`
+    position: relative;
+    padding: 20px 0;
+    max-width: 100%;
+    float: left;
+    margin-top: 2rem;
+`;
+
 interface InputInterface {
   id?: string;
-  value?: number;
+  value: number;
+  label?: string
   step?: string;
   setter: React.Dispatch<React.SetStateAction<number>>;
 }
 
-function Input({id, value, step, setter}:InputInterface) {
-  return <input
-            id={id}
-            type="number"
-            value={value}
-            className=""
-            min="0"
-            step={step}
-            onChange={
-              (e) => {
-                setter(parseFloat(e.currentTarget.value))
+function Input({id, value, label, step, setter}:InputInterface) {
+  return <FormGroupStyles>
+          <InputStyles
+              id={id}
+              type="number"
+              value={value ? value : ""}
+              min="0"
+              step={step}
+              onChange={
+                (e) => {
+                  setter(parseFloat(e.currentTarget.value))
+                }
               }
-            }
-          />;
+              required
+          />
+          <LabelStyles>{label}</LabelStyles>
+  </FormGroupStyles>
 }
 
 export default Input;
